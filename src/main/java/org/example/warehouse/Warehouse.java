8c39fc5e0f3a5f28b5f8d9d7e674e4383bc27120
alexa <alexander.wettergren.stenders@iths.se>
2025-01-13 22:30:04 +0100

    20 out of 25 tests passed. Only After adding multiple products tests remains.

--- src/main/java/org/example/warehouse/Warehouse.java
+++ src/main/java/org/example/warehouse/Warehouse.java
@@ -1,16 +1,24 @@
 package org.example.warehouse;
 
 
+import java.lang.reflect.UndeclaredThrowableException;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Warehouse {
 
-    private static Warehouse instance;
-    private String name;
-    private List<ProductRecord> productRecordList;
+    private static Map<String, Warehouse> createNewWarehouse = new HashMap<>();
+    private final String name;
+    private final List<ProductRecord> productRecordList;
+
+    public static Map<String, Warehouse> getCreateNewWarehouse() {
+        return createNewWarehouse;
+    }
+
+    public static void setNewWarehouse(Map<String, Warehouse> createNewWarehouse) {
+        Warehouse.createNewWarehouse = createNewWarehouse;
+    }
 
     public String getName() {
         return name;
@@ -21,24 +29,42 @@ public class Warehouse {
         this.productRecordList = new ArrayList<>();
     }
 
-
-    public static Warehouse getInstance(String name) {
-        if (instance == null)
-            instance = new Warehouse(name);
-        else if (!instance.getName().equals(name))
-            throw new IllegalArgumentException("Error");
-        return instance;
+    public static void createWarehouse(String name) {
+        createNewWarehouse.put(name, new Warehouse(name));
     }
 
     public static Warehouse getInstance() {
-        if (instance == null) {
-            throw new IllegalArgumentException("En warehouse har inte skapats än");
+        createWarehouse("Default warehouse");
+        return createNewWarehouse.get("Default warehouse");
+    }
+    public static Warehouse getInstance(String name) {
+        if(!createNewWarehouse.containsKey(name)) {
+            createWarehouse(name);
         }
-        return instance;
+        return createNewWarehouse.getOrDefault(name, new Warehouse(name));
     }
 
     public ProductRecord addProduct(UUID uuid, String productName, Category category, BigDecimal price) {
+        if (productName == null) {
+            throw new IllegalArgumentException("Product name can't be null or empty.");
+        } else if(productName.isEmpty()) {
+            throw new IllegalArgumentException("Product name can't be null or empty.");
+        } else if(category == null) {
+            throw new IllegalArgumentException("Category can't be null.");
+        }
+        if(uuid == null) {
+            uuid = UUID.randomUUID();
+        }
+        if(price == null) {
+            price = BigDecimal.ZERO;
+        }
         ProductRecord product = new ProductRecord(uuid,productName,category,price);
+        for(ProductRecord productRecord : productRecordList) {
+            if(product.uuid().equals(productRecord.uuid())) {
+                throw new IllegalArgumentException("Product with that id already exists, use updateProduct for updates.");
+            }
+
+        }
         this.productRecordList.add(product);
         return product;
     }
@@ -51,21 +77,44 @@ public class Warehouse {
         return productRecordList.isEmpty();
     }
 
-    public List<ProductRecord>  getProductById(UUID productId) {
-        return productRecordList;
+    public Optional<ProductRecord> getProductById(UUID productId) {
+        return productRecordList.stream().filter(product -> product.uuid().equals(productId)).findFirst();
     }
 
     public void updateProductPrice(UUID productId, BigDecimal price) {
-        ProductRecord product = productRecordList.getFirst();
-        ProductRecord updateProductsPrice = new ProductRecord(productId, product.productName(), product.category(), price);
-        int theSoleProduct = productRecordList.indexOf(product);
-        productRecordList.set(theSoleProduct, updateProductsPrice);
+        getProductById(productId).ifPresentOrElse(product -> {ProductRecord updatedProductPrice = new ProductRecord(productId, product.productName(), product.category(),price);
+        int theProductThatPriceWillChange = productRecordList.indexOf(product);
+            if(theProductThatPriceWillChange != -1) {
+                productRecordList.set(theProductThatPriceWillChange, updatedProductPrice);
+            }
+        },
+                () -> {
+                    throw new IllegalArgumentException("Product with that id doesn't exist.");
+                }
+        );
     }
 
     public List<ProductRecord> getChangedProducts(){
         return getProducts();
     }
 
+    public Map<Category, List<ProductRecord>> getProductsGroupedByCategories() {
+        return productRecordList.stream().collect(Collectors.groupingBy(ProductRecord::category));
+    }
+
+    public List<ProductRecord> getProductsBy(Object theThingItLooksFor) {
+        return switch (theThingItLooksFor) {
+            case Category searchCategory ->
+                    productRecordList.stream().filter(product -> product.category().equals(searchCategory)).collect(Collectors.toList());
+            case String searchName ->
+                    productRecordList.stream().filter(product -> product.productName().equals(searchName)).collect(Collectors.toList());
+            case UUID searchUUID ->
+                    productRecordList.stream().filter(product -> product.uuid().equals(searchUUID)).collect(Collectors.toList());
+            case BigDecimal searchPrice ->
+                    productRecordList.stream().filter(product -> product.price().equals(searchPrice)).collect(Collectors.toList());
+            case null, default -> throw new IllegalArgumentException("This thing in the record does not exists");
+        };
+    }
     public static void main(String[] args) {
 
     }
